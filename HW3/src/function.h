272695afecba128a98481cda8372d55c37f40721
Vickiewwu <146940276+Vickiewwu@users.noreply.github.com>
2024-03-05 16:29:23 +0800

    Create function.h

---
+++ HW3/src/function.h
@@ -0,0 +1,8 @@
+#ifndef EXAMPLE_FUNCTIONS_H
+#define EXAMPLE_FUNCTIONS_H
+
+extern std::vector<unsigned int>hcontour;
+extern std::vector<unsigned int>lcontour;
+extern std::vector<unsigned int>tcontour;
+extern std::map<std::string,unsigned int> mixedmap;
+#endif 
